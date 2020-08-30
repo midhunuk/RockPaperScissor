ce59936fcc55be1b236df61f04c01b12c8c9f112
midhunuk <midhunukumar5@gmail.com>
2020-08-30 14:54:42 +0530

    Added Start page

---
+++ RockPaperScissor/RPSGUI/Converter/BoolToVisibilityConverter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace RPSGUI.Converter
+{
+    public class BoolToVisibilityConverter : IValueConverter
+    {
+        public bool IsInverted { get; set; }
+
+        public bool IsCollapsed { get; set; }
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if(!(value is bool))
+            {
+                throw new ArgumentException("Argumnet is not boolean type");
+            }
+
+            var input = IsInverted ? !(bool)value : (bool)value;
+
+            if(input)
+            {
+                return Visibility.Visible;
+            }
+            else
+            {
+                return IsCollapsed ? Visibility.Collapsed : Visibility.Hidden;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
--- RockPaperScissor/RPSGUI/MainWindowViewModel.cs
+++ RockPaperScissor/RPSGUI/MainWindowViewModel.cs
@@ -3,7 +3,6 @@ using GalaSoft.MvvmLight.Command;
 using RPSBase;
 using RPSBase.Model;
 using System;
-using System.Windows.Input;
 
 namespace RPSGUI
 {
@@ -13,30 +12,52 @@ namespace RPSGUI
         private string cpuTurns;
         private string result;
         private Random random;
+        private string playerOneNameInput;
+        private bool isStartViewShown;
 
         public MainWindowViewModel(IRpsGame rpsGame)
         {
+            this.isStartViewShown = true;
             this.rPSGame = rpsGame;
             this.random = new Random();
             this.PlayerOneMoveCommand = new RelayCommand<string>(this.PlayGame);
             this.ResetGameCommand = new RelayCommand(this.ResetGame);
+            this.StartGameCommand = new RelayCommand(this.StartGame, this.CanExecuteStartGameCommand);
         }
 
-        public string CpuTurns 
-        { 
-            get => cpuTurns; 
-            set => this.Set(nameof(this.CpuTurns), ref this.cpuTurns, value); 
+        public string CpuTurns
+        {
+            get => cpuTurns;
+            set => this.Set(nameof(this.CpuTurns), ref this.cpuTurns, value);
         }
 
-        public string Result 
-        { 
+        public string Result
+        {
             get => result;
             set => this.Set(nameof(this.Result), ref this.result, value);
+        }
+
+        public string PlayerOneNameInput 
+        { 
+            get => playerOneNameInput;
+            set 
+            {
+                this.Set(nameof(this.playerOneNameInput), ref this.playerOneNameInput, value);
+                this.StartGameCommand.RaiseCanExecuteChanged();
+            }
+        }
+
+        public bool IsStartViewShown
+        {
+            get => isStartViewShown;
+            set => this.Set(nameof(this.isStartViewShown), ref this.isStartViewShown, value);
         }
 
         public RelayCommand<string> PlayerOneMoveCommand { get; }
 
         public RelayCommand ResetGameCommand { get; }
+
+        public RelayCommand StartGameCommand { get; }
 
         public string PlayerOneName => rPSGame.PlayerOneName;
 
@@ -46,9 +67,22 @@ namespace RPSGUI
 
         public double PlayerTwoScore => rPSGame.PlayerTwoScore;
 
+        private void StartGame()
+        {
+            this.ResetGame();
+            this.rPSGame.SetPlayerName(this.PlayerOneNameInput);
+            this.IsStartViewShown = false;
+            this.NameChanged();
+        }
+
+        private bool CanExecuteStartGameCommand()
+        {
+            return !string.IsNullOrEmpty(this.PlayerOneNameInput);
+        }
+
         private void PlayGame(string playerOneMove)
         {
-            var cpuMove = this.random.Next(0,3);
+            var cpuMove = this.random.Next(0, 3);
             var cpu = (RPS)cpuMove;
             var playerOne = (RPS)Enum.Parse(typeof(RPS), playerOneMove);
             this.rPSGame.Game(playerOne, cpu);
@@ -63,6 +97,13 @@ namespace RPSGUI
             this.CpuTurns = string.Empty;
             this.Result = this.rPSGame.Result;
             this.ScoreChanged();
+            this.NameChanged();
+        }
+
+        private void NameChanged()
+        {
+            this.RaisePropertyChanged(nameof(PlayerOneName));
+            this.RaisePropertyChanged(nameof(PlayerTwoName));
         }
 
         private void ScoreChanged()
